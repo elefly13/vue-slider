052690e9d7a473e2bab9109a8974f87503c7b5fd
Elefly13 <elymara13@gmail.com>
2021-09-08 17:23:25 +0200

    work in progress

--- js/main.js
+++ js/main.js
@@ -1,12 +1,26 @@
 Vue.config.devtools = true;
 
-
-
 const app = new Vue (
     {
         el: '#root',
         data: {
-
+            contatoreFoto: 0,
+            boxFoto: [
+                'img/image1.jpg',
+                'img/image2.jpg',
+                'img/image3.jpg',
+                'img/image4.jpg',
+            ]
+        },
+        methods: {
+            precFoto() {
+                this.contatoreFoto -= 1;
+                if (this.contatoreFoto < 0) this.contatoreFoto = (this.boxFoto.length -1);
+            },
+            succFoto() {
+                this.contatoreFoto += 1;
+                if (this.contatoreFoto == (this.boxFoto.length)) this.contatoreFoto = 0;
+            }
         }
     }
 )
