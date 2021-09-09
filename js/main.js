c583e729ecd13eea8322d8a6176cd7ff71f33f80
Elefly13 <elymara13@gmail.com>
2021-09-09 15:18:58 +0200

    aggiunta funzionalità lezione

--- js/main.js
+++ js/main.js
@@ -11,15 +11,28 @@ const app = new Vue (
                 'img/image2.jpg',
                 'img/image3.jpg',
                 'img/image4.jpg',
-            ]
+            ],
+            playVisible: false,
         },
-        mounted: function() {
-            this.startSlide();
+        mounted() {
+            // this.startSlide(); ..... //metodo 1
+
+            // setInterval(() => {
+            //     this.succFoto();
+            // }, 3000);  ..... //metodo 2
+
+            document.addEventListener('keydown', (e) => {  //funzione che mi permette di usare le frecce della tastiera per cambiare le foto
+                if (e.key == 'ArrowRight') {
+                    this.succFoto();
+                } else if (e.key == 'ArrowLeft') {
+                    this.precFoto();
+                }
+            });
         },
         methods: {
-            startSlide: function() {
-                this.timer = setInterval(this.succFoto, 3000);
-            },
+            // startSlide() {
+            //     this.timer = setInterval(this.succFoto, 3000);
+            // },  ..... //metodo 1
             
             precFoto() {
                 this.contatoreFoto -= 1;
@@ -28,8 +41,23 @@ const app = new Vue (
             succFoto() {
                 this.contatoreFoto += 1;
                 if (this.contatoreFoto == (this.boxFoto.length)) this.contatoreFoto = 0;
+            },
+            toggleVisibility() {
+                this.playVisible = !this.playVisible;
+                this.startStopSlider();
+            },
+            startStopSlider(){
+                if(!this.playVisible) {
+                    this.timer = setInterval(() => {
+                        this.succFoto();
+                    }, 3000);
+                } else {
+                    clearInterval(this.timer);
+                }
+            },
+            changePhoto(index) {
+                this.contatoreFoto = index;
             }
-            
         }
     }
 )
